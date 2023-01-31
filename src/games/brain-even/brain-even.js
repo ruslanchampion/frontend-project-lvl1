94f9f0a3ed0d2a128957da6001704f3391c0ef20
Ruslan Timbakov <ruslan.timbakov@gmail.com>
2023-01-31 10:21:09 +0300

    fix game rules

--- src/games/brain-even/brain-even.js
+++ src/games/brain-even/brain-even.js
@@ -2,7 +2,7 @@ import { brainGame } from "../../index.js"
 import { getQA } from "./getQA.js"
 
 export function brainEven() {
-  const gameRules = "Answer 'yes' if the number is even, otherwise answer 'no'."
+  const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".'
 
   brainGame(gameRules, getQA)
 }
