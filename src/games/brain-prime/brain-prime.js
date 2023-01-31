ce9e5b7816418e536d7096aa4c3565f6684a3094
Ruslan Timbakov <ruslan.timbakov@gmail.com>
2023-01-31 14:39:38 +0300

    fix quotes in prime game

--- src/games/brain-prime/brain-prime.js
+++ src/games/brain-prime/brain-prime.js
@@ -2,6 +2,6 @@ import { brainGame } from "../../index.js"
 import { getQA } from "./getQA.js"
 
 export function brainPrime() {
-  const gameRule = "Answer 'yes' if given number is prime. Otherwise answer 'no'."
+  const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".'
   brainGame(gameRule, getQA)
 }
